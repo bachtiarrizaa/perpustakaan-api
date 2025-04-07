4aa954f791fce8d72c164dfad545cef90f3d7054
bachtiarrizaa <bachtiarriza87@gmail.com>
2025-04-07 22:31:58 +0700

    register

---
+++ src/controllers/auth/register.js
@@ -0,0 +1,69 @@
+const bcrypt = require('bcrypt');
+const { User, Role } = require('../../models');
+
+const register = async (req, res, next) => {
+  try {
+    if (!req.body || !req.body.name || !req.body.email || !req.body.password) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Semua field harus diisi',
+      });
+    }
+
+    const name = req.body.name.trim();
+    const email = req.body.email.trim().toLowerCase();
+    const password = req.body.password.trim();
+
+    if (password.length < 8) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Password harus terdiri dari minimal 8 karakter'
+      });
+    }
+    
+
+    const findEmail = await User.findOne({ where: { email } });
+
+    if (findEmail) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Email sudah ada, gunakan yang lain'
+      });
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const defaultRoleId = 1;
+
+    const roleExist = await Role.findByPk(defaultRoleId);
+    if (!roleExist) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Role tidak ditemukan, hubungi admin'
+      });
+    }
+
+    const created = await User.create({
+      name,
+      email,
+      password: hashPassword,
+      role_id: defaultRoleId,
+    });
+
+    return res.status(201).json({
+      status: 'success',
+      message: 'Berhasil melakukan registrasi',
+      data: {
+        id: created.id,
+        name: created.name,
+        email: created.email,
+        role: roleExist.name,
+      }
+    })
+  } catch(error) {
+    console.log(error);
+    next(error)
+  }
+};
+
+module.exports = register;
--- src/server.js
+++ src/server.js
@@ -1,1 +1,42 @@
-console.log("Hello World");
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+const morgan = require('morgan');
+const app = express();
+const routes = require('./routes');
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+app.use('/api', routes);
+
+app.get('/', (req, res, next) => {
+  try {
+    return res.send("Welcome to My API");
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((req, res) => {
+  return res.status(404).json({
+    status: 'fail',
+    message: 'not found'
+  });
+});
+
+app.use((err, req, res) => {
+  return res.status(500).json({
+    status: 'fail',
+    message: 'Internal server error' + err.message,
+    data: null
+  });
+});
+
+
+app.listen(PORT, () => {
+  return console.log(`Server berjalan di http://localhost:${PORT}`);
+});
