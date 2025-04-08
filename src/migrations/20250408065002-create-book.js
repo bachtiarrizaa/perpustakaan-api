a920f45a0bd17a1c2624f8ee5c631fd2221a5621
bachtiarrizaa <bachtiarriza87@gmail.com>
2025-04-08 14:10:36 +0700

    book author publisher migrations

---
+++ src/migrations/20250408065002-create-book.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      year: {
+        type: Sequelize.INTEGER
+      },
+      stock: {
+        type: Sequelize.INTEGER
+      },
+      author_id: {
+        type: Sequelize.INTEGER
+      },
+      publisher_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Books');
+  }
+};
---
+++ src/models/book.js
@@ -0,0 +1,37 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Book extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Book.belongsTo(models.Author, {
+        foreignKey: 'author_id',
+        as: 'author'
+      });
+
+      Book.belongsTo(models.Publisher, {
+        foreignKey: 'publisher_id',
+        as: 'publisher'
+      });
+    }
+  }
+  Book.init({
+    name: DataTypes.STRING,
+    title: DataTypes.STRING,
+    year: DataTypes.INTEGER,
+    stock: DataTypes.INTEGER,
+    author_id: DataTypes.INTEGER,
+    publisher_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Book',
+  });
+  return Book;
+};
